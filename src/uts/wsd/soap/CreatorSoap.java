07ea8acd376ec0a6f088639bc163d80e6cf5bd42
Gabriel Mojica <niko.mojica@live.com>
2016-05-24 18:11:22 +1000

    create soap for creators

---
+++ src/uts/wsd/soap/CreatorSoap.java
@@ -0,0 +1,43 @@
+package uts.wsd.soap;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.servlet.ServletContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+
+import uts.wsd.Creator;
+import uts.wsd.CreatorBean;
+
+@WebService
+public class CreatorSoap {
+
+	public CreatorSoap() {
+	}
+
+	@Resource
+	private WebServiceContext context;
+	
+	private CreatorBean getCreatorBean() throws JAXBException, IOException {
+		ServletContext application = (ServletContext)context.getMessageContext().get(MessageContext.SERVLET_CONTEXT);
+		synchronized (application) {
+			CreatorBean creatorBean = (CreatorBean) application.getAttribute("diaryApp");
+			if (creatorBean == null) {
+				creatorBean = new CreatorBean();
+				creatorBean.setCreatorFilePath(application.getRealPath("WEB-INF/creators.xml"));
+				application.setAttribute("creatorBean", creatorBean);
+			}
+			return creatorBean;
+		}
+	}
+
+	@WebMethod
+	public Creator login(String username, String password) throws JAXBException, IOException {
+		return getCreatorBean().getCreators().login(username, password);
+	}
+
+}
